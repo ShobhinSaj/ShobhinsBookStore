646fdc8bfa2fe0e76b0d5bfd5f564e71ee94497c
madmax117 <maxjonas117@gmail.com>
2023-11-06 19:27:21 -0500

    Migration added and database updated with commands "Add-Migration DefaultIdentityMigrationAdded";"update-database"

---
+++ ShobhinsBooks.DataAccess/Data/Migrations/20231107002113_DefaultIdentityMigrationAdded.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShobhinsBookStore.Data.Migrations
+{
+    public partial class DefaultIdentityMigrationAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
